f62dace77f947af0207dad9be453f0b541e6d301
Абдуманап уулу Жорупбек <ZAbdumanap-uulu@sberbank.ru>
2022-05-22 15:35:26 +0300

    ready

--- rush00/settings.py
+++ rush00/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-kp02969(bpw+yk_uo2ne3p4g4u#!*p_5t3vdnrjgast2nq31p-
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -73,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'rush00.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -83,7 +80,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -103,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -114,7 +109,6 @@ TIME_ZONE = 'Europe/Moscow'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
@@ -137,17 +131,17 @@ MOVIEMON = [
             'x': 0,
             'y': 0
         },
-        'IMDB_title' : {
-            0 : "True Grit",
-            1 : "Sinbad: The Fifth Voyage",
-            2 : "Jaguar: Part 2",
-            3 : "Breathe Deeply",
-            4 : "Justice League",
-            5 : "Epic Movie",
-            6 : "Glitter",
-            7 : "Alfred the Great",
-            8 : "Back to school",
-            9 : "Disaster Movie",
+        'IMDB_title': {
+            0: "True Grit",
+            1: "Sinbad: The Fifth Voyage",
+            2: "Jaguar: Part 2",
+            3: "Breathe Deeply",
+            4: "Justice League",
+            5: "Epic Movie",
+            6: "Glitter",
+            7: "Alfred the Great",
+            8: "Back to school",
+            9: "Disaster Movie",
         }
     }
 ]
